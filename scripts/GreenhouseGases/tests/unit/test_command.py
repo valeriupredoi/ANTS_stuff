d4b19dc0744a7bd8428978f881c777b71f604733
Valeriu Predoi <valeriu.predoi@gmail.com>
2023-11-03 13:44:03 +0000

    add cmdline test for greenhouse

---
+++ scripts/GreenhouseGases/tests/unit/test_command.py
@@ -0,0 +1,65 @@
+"""Tests for NitrogenDeposition CLI.
+
+Includes a context manager to temporarily modify sys.argv
+requirement: Python 3.7
+"""
+import subprocess
+import contextlib
+import copy
+import functools
+import sys
+
+from pathlib import Path
+from unittest.mock import patch
+from io import StringIO
+
+import pytest
+
+from GreenhouseGases import GHG_radiation
+from GreenhouseGases import ghg_ukca_ncplot
+from GreenhouseGases import GHG_UKCA
+
+
+def test_run_GHG_radiation_command():
+    """Test run command."""
+    executable = str(Path(GHG_radiation.__file__))
+    print(f"Path to executable: {executable}")
+    cmd = [executable, '--help']
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    stdoutdata, stderrdata = process.communicate()
+    assert process.returncode == 0
+    cmd = [executable, '--cow']
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    stdoutdata, stderrdata = process.communicate()
+    assert process.returncode == 2
+
+
+@pytest.mark.skip(
+    reason='Cannot run executable without access to data.'
+)
+def test_run_ghg_ukca_ncplot_command():
+    """Test run command."""
+    executable = str(Path(ghg_ukca_ncplot.__file__))
+    print(f"Path to executable: {executable}")
+    cmd = [executable, '--help']
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    stdoutdata, stderrdata = process.communicate()
+    assert process.returncode == 0
+    cmd = [executable, '--cow']
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    stdoutdata, stderrdata = process.communicate()
+    assert process.returncode == 2
+
+
+def test_run_GHG_UKCA_command():
+    """Test run command."""
+    executable = str(Path(GHG_UKCA.__file__))
+    print(f"Path to executable: {executable}")
+    cmd = [executable, '--help']
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    stdoutdata, stderrdata = process.communicate()
+    assert process.returncode == 0
+    cmd = [executable, '--cow']
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    stdoutdata, stderrdata = process.communicate()
+    assert process.returncode == 2
